33e4e35402b799cb9bdfe999f139bc5a34255186
Franco Ortega <francopdx@gmail.com>
2024-08-20 15:24:41 -0700

    update GET request to reach level 1

--- app/api/proxy/route.js
+++ app/api/proxy/route.js
@@ -1,18 +1,45 @@
 export const dynamic = 'force-static';
 
-export async function GET(bundle) {
-	console.log(bundle);
+const BASE_URL = 'https://ciphersprint.pulley.com';
+const EMAIL = 'francoortegadev@gmail.com';
 
-	const res = await fetch(
-		'https://ciphersprint.pulley.com/francoortegadev@gmail.com',
-		{
-			headers: {
-				'Content-Type': 'application/json',
-				'API-Key': process.env.DATA_API_KEY,
-			},
-		}
-	);
-	const data = await res.json();
+const updateUrl = (data) => {
+	if (data.encryption_method === 'encoded as base64') {
+		const chunk = path.slice(5);
+		console.log({ chunk });
+		const decodedChunk = btoa(chunk);
+		console.log({ decodedChunk });
+		const newPath = `task_${decodedChunk}`;
+		return newPath;
+	}
+
+	return data.encrypted_path;
+};
+
+export async function GET() {
+	const res = await fetch(`${BASE_URL}/${EMAIL}`, {
+		headers: {
+			'Content-Type': 'application/json',
+			'API-Key': process.env.DATA_API_KEY,
+		},
+	});
+
+	const firstData = await res.json();
+
+	const NEXT_PATH = updateUrl(firstData);
+
+	console.log({ NEXT_PATH });
+
+	const newRes = await fetch(`${BASE_URL}/${NEXT_PATH}`, {
+		headers: {
+			'Content-Type': 'application/json',
+			'API-Key': process.env.DATA_API_KEY,
+		},
+	});
+
+	const data = await newRes.json();
+
+	console.log({ data });
 
 	return Response.json({ data });
 }
