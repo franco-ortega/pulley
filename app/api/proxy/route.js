0572cea372a6d95be832d1ccebdcfd0f2c71330e
Franco Ortega <francopdx@gmail.com>
2024-08-21 14:38:22 -0700

    clean up code

--- app/api/proxy/route.js
+++ app/api/proxy/route.js
@@ -32,60 +32,31 @@ const addToAcsii = (chars, num) => {
 };
 
 const decodeHex = (encodedString, keyHolder) => {
-	// hex
-	// hex decoded
-	// encrypt with XOR
-	// hex encoded again
-
-	// decode hex        -> HEX to STRING
-	// decrypt with XOR  -> STRING decrypt
-	// encode hex        -> STRING to HEX
-
 	const keyLocation = keyHolder.split('').findIndex((char) => char === ':');
 	const key = keyHolder.slice(keyLocation + 2);
 
-	console.log({ key });
-
-	// Step 1: Hex Decode
 	function hexToBytes(hex) {
 		let bytes = [];
 		for (let c = 0; c < hex.length; c += 2) {
 			bytes.push(parseInt(hex.substr(c, 2), 16));
 		}
 
-		// let bytes = '';
-		// for (let i = 0; i < hex.length; i += 2) {
-		// 	const hexValue = hex.substr(i, 2);
-		// 	const decimalValue = parseInt(hexValue, 16);
-		// 	bytes += String.fromCharCode(decimalValue);
-		// }
-
 		return bytes;
 	}
 
 	const decodedBytes = hexToBytes(encodedString);
-	console.log({ decodedBytes });
 
 	function xorDecrypt(bytes, key) {
 		let decrypted = '';
 		for (let i = 0; i < bytes.length; i++) {
 			const keyCode = key.charCodeAt(i % key.length);
 			const decryptedByte = bytes[i] ^ keyCode;
-			console.log('KEY CODE: ', keyCode);
-			console.log('BYTE: ', bytes[i]);
-			console.log('DECRYPTED BYTE: ', decryptedByte);
-			console.log('STRING: ', String.fromCharCode(decryptedByte));
-			// decrypted += String.fromCharCode(decryptedByte);
-			// const THING = String.fromCharCode(decryptedByte);
-			// console.log({ THING });
 			decrypted += decryptedByte.toString(16);
 		}
 		return decrypted;
 	}
 
 	const decryptedMessage = xorDecrypt(decodedBytes, key);
-
-	console.log({ LLLLLLL: decryptedMessage }); // Output the decrypted message
 
 	return decryptedMessage;
 };
@@ -107,7 +78,6 @@ const updateUrlSegment = (encryptionMethod, encryptedUrlSegment) => {
 
 		case 'hex decoded, encrypted with XOR, hex encoded again. key: secret':
 			const decodedUrlSegment = decodeHex(clippedUrlSegment, encryptionMethod);
-			console.log(decodedUrlSegment);
 			return `task_${decodedUrlSegment}`;
 
 		default:
@@ -128,14 +98,10 @@ const goDownTheRabbitHole = async (urlSegment) => {
 
 	logCount(res.level);
 
-	console.log(res);
-
 	const updatedUrlSegment = updateUrlSegment(
 		res.encryption_method,
 		res.encrypted_path
 	);
-
-	console.log({ updatedUrlSegment });
 
 	if (updatedUrlSegment === 'DEAD_END') return res;
 
